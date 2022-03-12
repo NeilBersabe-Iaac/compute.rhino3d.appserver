75653dcfc18d0dcf70e03bcf0657a1bc75cda5d8
ArchSaber22 <47876822+TruLuvW9s92@users.noreply.github.com>
2022-03-12 10:43:28 +0800

    Working v.02 added spaces to the parameters

--- src/examples/solihiya00_v2/script.js
+++ src/examples/solihiya00_v2/script.js
@@ -46,15 +46,15 @@ lrgOpening_slider.addEventListener("touchend", onSliderChange, false);
 const Showtrees = document.getElementById("Show Trees");
 Showtrees.addEventListener("change", onSliderChange, false);
 
-const TreesNo = document.getElementById("RH_IN:Trees No");
+const TreesNo = document.getElementById("RH_IN: Trees No");
 TreesNo.addEventListener("mouseup", onSliderChange, false);
 TreesNo.addEventListener("touchend", onSliderChange, false);
 
-const TreesLocation = document.getElementById("RH_IN:Trees Location");
+const TreesLocation = document.getElementById("RH_IN: Trees Location");
 TreesLocation.addEventListener("mouseup", onSliderChange, false);
 TreesLocation.addEventListener("touchend", onSliderChange, false);
 
-const TreesScale = document.getElementById("RH_IN:Trees Scale");
+const TreesScale = document.getElementById("RH_IN: Trees Scale");
 TreesScale.addEventListener("mouseup", onSliderChange, false);
 TreesScale.addEventListener("touchend", onSliderChange, false);
 
@@ -63,11 +63,11 @@ TreesScale.addEventListener("touchend", onSliderChange, false);
 const ShowPeople = document.getElementById("Show People");
 ShowPeople.addEventListener("change", onSliderChange, false);
 
-const Population = document.getElementById("RH_IN:Population");
+const Population = document.getElementById("RH_IN: Population");
 Population.addEventListener("mouseup", onSliderChange, false);
 Population.addEventListener("touchend", onSliderChange, false);
 
-const PeopleLocation = document.getElementById("RH_IN:People Location");
+const PeopleLocation = document.getElementById("RH_IN: People Location");
 PeopleLocation.addEventListener("mouseup", onSliderChange, false);
 PeopleLocation.addEventListener("touchend", onSliderChange, false);
 
