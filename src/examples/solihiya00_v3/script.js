0c2f3962213c8aeb2990bd1206e9dc5f6a73c001
NeilBersabe-Iaac <neil.bersabe@students.iaac.net>
2022-03-13 18:19:07 +0800

    Stable Release v3.ghv4

--- src/examples/solihiya00_v3/script.js
+++ src/examples/solihiya00_v3/script.js
@@ -105,23 +105,23 @@ rhino3dm().then(async (m) => {
 
 //   const startPts = [
 
-//     {x:3.276063, y:11.223933, z:0},
+//     {x:3.276063, y: 11.223933, z: 0},
 //     {x:-8.663308, y:18.347937, z:0},
-//     {x:-3.87914, y:33.307085, z:0},
+//     {x:1.933446, y:29.818253, z:0},
 //     {x:4.563591, y:44.35353, z:0},
 //     {x:16.825266, y:45.566347, z:0},
 //     {x:21.027902, y:38.132332, z:0},
-//     {x:28.596457, y:34.535895, z:0},
+//     {x:28.596457, y:28.180323, z:0},
 //     {x:37.259009, y:35.357609, z:0},
 //     {x:44.208391, y:30.493998, z:0},
-//     {x:45.483061, y:23.416053, z:0},
+//     {x:45.483061, y:18.291841, z:0},
 //     {x:41.160147, y:7.166575, z:0},
-//     {x:31.918529, y:5.326137, z:0},
+//     {x:31.918529, y:7.766827, z:0},
 //     {x:24.132118, y:6.224273, z:0},
 //     {x:21.344111, y:9.850299, z:0},
-//     {x:18.25342, y:16.528613, z:0},
-
+//     {x:18.25342, y:16.528613, z:0}
 //   ]
+
 // const cntPts = startPts.length;
 
 //   for (let i = 0; i < cntPts; i++) {
@@ -570,8 +570,11 @@ function showSpinner(enable) {
 
 // Boilerplate//
 // more globals
-var scene, camera, renderer, controls;
+var scene, camera, renderer, controls, raycaster, selectedMaterial;
  
+// const mouse = new THREE.Vector2()
+// window.addEventListener( 'click', onClick, false);
+
 /**
  * Sets up the scene, camera, renderer, lights and controls and starts the animation
  */
@@ -648,6 +651,96 @@ function init() {
 }
 
 
+
+
+/////////////////////////
+//Select Raycaster
+/////////////////////////
+
+// selectedMaterial = new THREE.MeshStandardMaterial( {color: 'darkorange'} )
+// raycaster = new THREE.Raycaster()
+
+
+
+// function onClick( event ) {
+
+// console.log( `click! (${event.clientX}, ${event.clientY})`)
+
+// // calculate mouse position in normalized device coordinates
+// // (-1 to +1) for both components
+
+// mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1
+// mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1
+
+// raycaster.setFromCamera( mouse, camera )
+
+// // calculate objects intersecting the picking ray
+// const intersects = raycaster.intersectObjects( scene.children, true )
+
+// let container = document.getElementById( 'container' )
+// if (container) container.remove()
+
+// // reset object colours
+// scene.traverse((child, i) => {
+//     if (child.userData.hasOwnProperty( 'material' )) {
+//         child.material = child.userData.material
+//     }
+// })
+
+// if (intersects.length > 0) {
+
+//     // get closest object
+//     const object = intersects[0].object
+//     console.log(object) // debug
+
+//     object.traverse( (child) => {
+//         if (child.parent.userData.objectType === 'Brep') {
+//             child.parent.traverse( (c) => {
+//                 if (c.userData.hasOwnProperty( 'material' )) {
+//                     c.material = selectedMaterial
+//                 }
+//             })
+//         } else {
+//             if (child.userData.hasOwnProperty( 'material' )) {
+//                 child.material = selectedMaterial
+//             }
+//         }
+//     })
+
+//     // get user strings
+//     let data, count
+//     if (object.userData.attributes !== undefined) {
+//         data = object.userData.attributes.userStrings
+//     } else {
+//         // breps store user strings differently...
+//         data = object.parent.userData.attributes.userStrings
+//     }
+
+//     // do nothing if no user strings
+//     if ( data === undefined ) return
+
+//     console.log( data )
+    
+//     // create container div with table inside
+//     container = document.createElement( 'div' )
+//     container.id = 'container'
+    
+//     const table = document.createElement( 'table' )
+//     container.appendChild( table )
+
+//     for ( let i = 0; i < data.length; i ++ ) {
+
+//         const row = document.createElement( 'tr' )
+//         row.innerHTML = `<td>${data[ i ][ 0 ]}</td><td>${data[ i ][ 1 ]}</td>`
+//         table.appendChild( row )
+//     }
+
+//     document.body.appendChild( container )
+// }
+
+// }
+////////////////////////
+////////////////////////
 /**
  * The animation loop!
  */
